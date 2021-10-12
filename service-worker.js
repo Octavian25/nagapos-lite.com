048c3bea63a9a52391c1a74289bd5ed6d82c3f5c
Octavian <octaviansaja2@gmail.com>
2021-10-12 11:27:27 +0700

    relayout penjualan & fix pembelian

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b80bab012e83281616cec77414765b90.js"
+  "./precache-manifest.435d74106be7e77f9cd8ce251286c528.js"
 );
 
 self.addEventListener('message', (event) => {
